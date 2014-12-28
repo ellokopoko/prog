1faa1d93e58730d46f569bb029f1f38ba461387d
Mike <ja-morning@yandex.ua>
2014-12-28 15:27:07 +0200

    update models and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_and_belongs_to_many :projects
-  has_many :task
-  has_many :comment
+  has_many :tasks
+  has_many :comments
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 >>>>>>> b5730778bc5778736634b4dd4e87e4b70d38d917
--- db/migrate/20141223092337_create_comments.rb
+++ db/migrate/20141223092337_create_comments.rb
@@ -1,8 +1,10 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :text
-      t.date :date
+      t.text    :text
+      t.date    :date
+      t.integer :task_id
+      t.integer :user_id
 
       t.timestamps null: false
     end
