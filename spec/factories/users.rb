f506859b1adfbfdadcbf096ee6ab8cd7603052c5
VlasovS <grishko.darya@gmail.com>
2014-12-29 09:53:28 +0200

    First design (layout and devise)

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,8 @@
 FactoryGirl.define do
   factory :user do
-<<<<<<< HEAD
-    loging "MyString"
-=======
     login "MyString"
->>>>>>> b5730778bc5778736634b4dd4e87e4b70d38d917
-email "MyString"
-password "MyString"
+    email "MyString"
+    password "MyString"
   end
 
 end
